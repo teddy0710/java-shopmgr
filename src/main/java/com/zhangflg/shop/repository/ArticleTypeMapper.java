8831f00461cab5875e5a4b71eb1b2d79691776d0
zhangflg <password123>
2018-04-26 12:25:08 +0800

    自动生成实体类 mapper及接口

---
+++ src/main/java/com/zhangflg/shop/repository/ArticleTypeMapper.java
@@ -0,0 +1,14 @@
+package com.zhangflg.shop.repository;
+
+/**
+ * ArticleTypeMapper 数据访问类
+ * @author xlei @qq 251425887 @tel 13352818008
+ * @Email dlei0009@163.com
+ * @date 2018-04-26 11:54:00
+ * @version 1.0
+ */
+public interface ArticleTypeMapper {
+
+
+
+}
